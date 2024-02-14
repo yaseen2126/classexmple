af4ae6ba3668f3dcb79b38a1bffd3c1630f43421
yaseen2126 <157618070+yaseen2126@users.noreply.github.com>
2024-02-14 21:11:50 +0500

    Add files via upload

---
+++ py..py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+st.title("Welcome Bano Qabil")
+
+#creating dataset
+
+df=pd.DataFrame({'first':[1,2,3,4,5,],'second':[6,7,8,9,10]})
+st.write(df)
+
+# or
+df  #magic comments
+st.area_chart(df)
